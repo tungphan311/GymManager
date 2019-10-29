6759559ecd6912dcac3653b13c7f2d732fd85bc0
unknown <thanhtunga1lqd@gmail.com>
2019-10-29 15:25:43 +0700

    setup router

--- src/index.js
+++ src/index.js
@@ -1,9 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Router, Switch, Route } from "react-router-dom";
+import history from "./state/history";
 import App from "./App";
 import "./styles/index.scss";
 import * as serviceWorker from "./serviceWorker";
+import Routes from "./routes/routes";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <Router history={history}>
+    <App>
+      <Switch>
+        <Route path="/" component={Routes} />
+      </Switch>
+    </App>
+  </Router>,
+  document.getElementById("root")
+);
 
 serviceWorker.unregister();
