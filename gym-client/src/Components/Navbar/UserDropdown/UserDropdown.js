36612694c6228aab1998e6be3618b94b88cd0e89
tungphan311 <thanhtunga1lqd@gmail.com>
2019-12-01 01:28:17 +0700

    fix wrong import

--- gym-client/src/Components/Navbar/UserDropdown/UserDropdown.js
+++ gym-client/src/Components/Navbar/UserDropdown/UserDropdown.js
@@ -1,5 +1,5 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
-import React from "Components/Navbar/UserDropdown/react";
+import React from "react";
 
 function UserDropdown({ ava = "/assets/img/profile.jpg" }) {
   return (
