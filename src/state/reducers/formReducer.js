386a40383dda7b159aafafb33bbc0d9fbbd00b70
lvhoang110298 <lvhoang119298@gmail.com>
2019-12-03 20:36:07 +0700

    init

--- src/state/reducers/formReducer.js
+++ src/state/reducers/formReducer.js
@@ -3,5 +3,6 @@ import { reducer } from "redux-form";
 // forms keys
 export const FORM_KEY_LOGIN = "form/FORM_KEY_LOGIN";
 export const FORM_KEY_FORGOT_PASSWORD = "form/FORM_KEY_FORGOT_PASSWORD";
+export const FORM_KEY_ADDSTAFF = "form/FORM_KEY_ADDSTAFF";
 
 export const formReducer = reducer;
