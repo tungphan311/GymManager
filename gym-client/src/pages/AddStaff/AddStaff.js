f2f58848219d4d1a6516bf4058a20b48c7e75d64
Phan Thanh Tùng <thanhtunga1lqd@gmail.com>
2019-12-03 21:30:55 +0700

    add datepicker

--- gym-client/src/pages/AddStaff/AddStaff.js
+++ gym-client/src/pages/AddStaff/AddStaff.js
@@ -1,33 +1,17 @@
 import React, { Component } from "react";
 import AddStaffForm from "pages/AddStaff/AddStaffForm";
 import "./AddStaff.scss";
-import DatePicker from "Components/DatePicker/DatePicker";
 
 class AddStaff extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      startDate: null
-    };
-  }
   handleSubmit = e => {
     e.preventDefault();
   };
 
   render() {
-    const { startDate } = this.state;
     return (
       <div className="addstaff">
         <h1 className="text-center">Add new Staff</h1>
         <AddStaffForm />
-        <DatePicker
-          // component={DatePicker}
-          startDate={startDate}
-          name="Birth"
-          color="red"
-          setStartDate={date => this.setState({ startDate: date })}
-        />
       </div>
     );
   }
