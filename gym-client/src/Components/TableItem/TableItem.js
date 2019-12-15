5c239c76e87c600a65e078592e14b58a7dee59ba
dangquang9a <dangquang9a@gmail.com>
2019-12-15 13:39:56 +0700

    Change to MDBDataTable

--- gym-client/src/Components/TableItem/TableItem.js
+++ gym-client/src/Components/TableItem/TableItem.js
@@ -1,22 +1,12 @@
 /* eslint-disable prettier/prettier */
 import React from "react";
-import { Table } from "antd";
+// import { Table } from "antd";
+import { MDBDataTable } from "mdbreact";
 import "./TableItem.scss";
 
-const TableItem = ({ dataSource, columns, rowSelection, onRow, onChange }) => (
+const TableItem = ({ dataSource }) => (
   <div className="table__container">
-    <Table
-      dataSource={dataSource}
-      columns={columns}
-      rowSelection={rowSelection}
-      onRow={onRow}
-      onChange={onChange}
-      scroll={{ y: 480 }}
-      pagination={{
-        pageSizeOptions: ["10", "20", "50", "100"],
-        showSizeChanger: true
-      }}
-    />
+    <MDBDataTable striped hover data={dataSource} />
   </div>
 );
 
