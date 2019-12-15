dcb428dedc0807e1cd367cd3b7e0d46bf2e20876
lvhoang110298 <lvhoang119298@gmail.com>
2019-12-15 14:48:51 +0700

    add staff form

--- gym-client/src/Components/DatePicker/DatePicker.js
+++ gym-client/src/Components/DatePicker/DatePicker.js
@@ -1,19 +1,29 @@
-import React, { Component } from "react";
+import React from "react";
 import ReactDatePicker from "react-date-picker";
 import "./DatePicker.scss";
 
-class DatePicker extends Component {
-  render() {
-    const { date, setDate, label } = this.props;
-    return (
-      <div>
-        <label>{label}</label>
-        <div className="md__date--picker--container">
-          <ReactDatePicker value={date} onChange={setDate} />
-        </div>
+const DatePicker = ({
+  label = "",
+  type = "date",
+  date = "",
+  setDate = "",
+  meta = {}, // redux form
+  input // redux form
+}) => {
+  const { touched, error } = meta;
+
+  const showError = touched && error;
+  const { errCode } = error || {};
+  // const { date, setDate, label } = this.props;
+
+  return (
+    <div>
+      <label>{label}</label>
+      <div className="md__date--picker--container">
+        <ReactDatePicker {...input} value={date} onChange={setDate} />
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default DatePicker;
--- src/state/reducers/formReducer.js
+++ src/state/reducers/formReducer.js
@@ -1,5 +1,11 @@
 import { reducer } from "redux-form";
 
+// const initState = {
+//     token: null,
+//     loginSuccess: false,
+//     identity: {}
+// };
+
 // forms keys
 export const FORM_KEY_LOGIN = "form/FORM_KEY_LOGIN";
 export const FORM_KEY_FORGOT_PASSWORD = "form/FORM_KEY_FORGOT_PASSWORD";
