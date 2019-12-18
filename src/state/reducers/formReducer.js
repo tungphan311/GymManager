55069309d7f4e2dcbbd7f55371c63ae34d99d1ab
Nguyễn Đăng Quang <dangquang9a@gmail.com>
2019-12-19 00:09:34 +0700

    ChangePassword Form

--- src/state/reducers/formReducer.js
+++ src/state/reducers/formReducer.js
@@ -3,5 +3,6 @@ import { reducer } from "redux-form";
 export const FORM_KEY_LOGIN = "form/FORM_KEY_LOGIN";
 export const FORM_KEY_FORGOT_PASSWORD = "form/FORM_KEY_FORGOT_PASSWORD";
 export const FORM_KEY_ADDSTAFF = "form/FORM_KEY_ADDSTAFF";
+export const FORM_KEY_CHANGE_PASSWORD = "form/FORM_KEY_CHANGE_PASSWORD";
 
 export const formReducer = reducer;
