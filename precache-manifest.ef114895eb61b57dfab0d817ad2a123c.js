77a8f7852be031e631146f8bacad3cce3c3a653d
tungphan311 <thanhtunga1lqd@gmail.com>
2019-12-30 21:11:26 +0700

    Updates

---
+++ precache-manifest.ef114895eb61b57dfab0d817ad2a123c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "947aca490d933ef445b8b4c9e4596461",
+    "url": "/GymManager/index.html"
+  },
+  {
+    "revision": "717350d1b870536b0aef",
+    "url": "/GymManager/static/css/2.e34a9976.chunk.css"
+  },
+  {
+    "revision": "c42597eca44f23a1b21e",
+    "url": "/GymManager/static/css/main.cfa57d83.chunk.css"
+  },
+  {
+    "revision": "717350d1b870536b0aef",
+    "url": "/GymManager/static/js/2.a0aaba55.chunk.js"
+  },
+  {
+    "revision": "c42597eca44f23a1b21e",
+    "url": "/GymManager/static/js/main.3e3042d2.chunk.js"
+  },
+  {
+    "revision": "bb6ac1e47f31dcc26b35",
+    "url": "/GymManager/static/js/runtime-main.aaf4fb64.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/public/precache-manifest.406ac54b2957f9abe32ed6dbc50af1af.js"
+  "/GymManager/precache-manifest.ef114895eb61b57dfab0d817ad2a123c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/public/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/GymManager/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
